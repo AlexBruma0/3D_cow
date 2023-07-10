a5f9bb11a885fc20a9a9def0e84d12870b6ba650
AlexBruma0 <105242355+AlexBruma0@users.noreply.github.com>
2023-07-10 15:35:51 -0700

    triangle rendered

--- main.js
+++ main.js
@@ -8,10 +8,12 @@ window.onload = async function init() {
     }
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.5, 0.5, 0.5, 1.0);
+    programBlack = initShaders(gl, "./shaders/static.vert", "./shaders/black.frag");
     render()
 }
 
 const render = () => {
     gl.clear(gl.COLOR_BUFFER_BIT);
+    cow()
     //window.requestAnimationFrame(render);
   };
